c9c7911361e49b7a876ca5b0be6c9ae2cf8a53d2
fretty233 <fretty.l.silalahi@gmail.com>
2022-01-25 18:43:25 +0700

    update

--- variable.go
+++ variable.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("")
+	var name string
+
+	name = "Fretty Silalahi"
+	fmt.Println(name)
+
+	name = "Fretty Mentari"
+	fmt.Println(name)
+
+	var word = "Silalahi"
+	fmt.Println(word)
+
+	var age int8 = 20
+	fmt.Println(age)
 }
