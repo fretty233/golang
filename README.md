9a8dff025073fe309f303ca4f426920edb281af2
fretty233 <fretty.l.silalahi@gmail.com>
2022-01-25 18:30:30 +0700

    day 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # belajar-golang
+# golang
---
+++ datatype.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("satu=", 1)
+	fmt.Println("dua=", 2)
+	fmt.Println("dua koma lima=", 2.5)
+
+	fmt.Println("tipe data boolean")
+	fmt.Println("benar =", true)
+	fmt.Println("salah =", false)
+
+	fmt.Println("tipe data string")
+	fmt.Println("Fretty")
+	fmt.Println("Fretty Silalahi")
+
+	fmt.Println(len("Fretty"))
+	fmt.Println("Fretty"[0])
+	fmt.Println("Fretty Silalahi"[1])
+}
